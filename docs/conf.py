792cfea18b249ee974296ba45db2db5a3967873c
Michael Penkov <misha.penkov@gmail.com>
2017-09-17 00:43:43 +0900

    update copyright statement

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'csvinsight'
-copyright = u"2017, Michael Penkov"
+copyright = u"2017, Profound Networks"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
